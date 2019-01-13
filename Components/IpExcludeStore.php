56d8ecf2be0a6ede567f1e45b6f450e3b7a246dc
David Neustadt <saberdude@googlemail.com>
2019-01-13 23:53:07 +0100

    Initial commit

---
+++ Components/IpExcludeStore.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace FroshHttpCacheIpExclude\Components;
+
+use Shopware\Components\HttpCache\BlackHoleStore;
+use Shopware\Components\HttpCache\Store;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class IpExcludeStore extends BlackHoleStore
+{
+    private $store;
+
+    /**
+     * @param array                                $options
+     * @param \Shopware\Kernel|HttpKernelInterface $kernel
+     */
+    public function __construct(
+        array $options,
+        HttpKernelInterface $kernel
+    ) {
+        if (
+            !empty($options['extended']['ipExcludes']) &&
+            is_array($options['extended']['ipExcludes']) &&
+            in_array($_SERVER['REMOTE_ADDR'], $options['extended']['ipExcludes'])
+        ) {
+            $this->store = $this;
+
+            return;
+        }
+
+        if (!empty($options['extended']['passedStoreClass'])) {
+            $class = $options['extended']['passedStoreClass'];
+
+            $this->store = new $class($options, $kernel);
+        } else {
+            $this->store = new Store(
+                $kernel->getCacheDir() . '/http_cache',
+                $options['cache_cookies'],
+                $options['lookup_optimization'],
+                $options['ignored_url_parameters']
+            );
+        }
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    function __get($name)
+    {
+        return $this->store->$name;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     */
+    function __set($name, $value)
+    {
+        $this->store->$name = $value;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $arguments
+     *
+     * @return mixed
+     */
+    function __call($name, $arguments)
+    {
+        return call_user_func_array([
+                $this->store,
+                $name,
+            ],
+            $arguments
+        );
+    }
+}
---
+++ FroshHttpCacheIpExclude.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace FroshHttpCacheIpExclude;
+
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Shopware\Components\Plugin\Context\UpdateContext;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class FroshHttpCacheIpExclude
+ */
+class FroshHttpCacheIpExclude extends Plugin
+{
+    const CONFIG_PHP_REQUIRE = 'require_once __DIR__ . \'/custom/plugins/FroshHttpCacheIpExclude/Components/IpExcludeStore.php\';';
+
+    CONST CONFIG_STORE_CLASS = 'FroshHttpCacheIpExclude\\Components\\IpExcludeStore';
+
+    const CONFIG_PHP_PROPERTY = "   'httpcache' => [
+        'storeClass' => 'FroshHttpCacheIpExclude\\Components\\IpExcludeStore',
+        'extended' => [
+            'passedStoreClass' => '',
+            'ipExcludes' => [],
+        ],
+    ],";
+
+    /**
+     * @param ActivateContext $context
+     *
+     * @throws \Exception
+     */
+    public function activate(ActivateContext $context)
+    {
+        $this->addConfigStoreClass();
+    }
+
+    /**
+     * @param DeactivateContext $context
+     */
+    public function deactivate(DeactivateContext $context)
+    {
+        $this->removeConfigStoreClass();
+    }
+
+    /**
+     * @param UninstallContext $context
+     */
+    public function uninstall(UninstallContext $context)
+    {
+        $this->removeConfigStoreClass();
+    }
+
+    private function getConfigPath()
+    {
+        return __DIR__ . '/../../../config.php';
+    }
+
+    private function hasConfigValue($value)
+    {
+        $config = require $this->getConfigPath();
+
+        return isset($config[$value]);
+    }
+
+    private function getConfigContent()
+    {
+        return file_get_contents($this->getConfigPath());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function addConfigStoreClass()
+    {
+        $configContent = $this->getConfigContent();
+        $search = 'return [';
+
+        if (!$this->hasConfigValue('httpcache')) {
+            $configContent = str_replace(
+                $search,
+                $search . "\r\n" . self::CONFIG_PHP_PROPERTY,
+                $configContent
+            );
+        } else {
+            $configContent = str_replace(
+                '\'storeClass\' => null',
+                '\'storeClass\' => \'' . self::CONFIG_STORE_CLASS . '\'',
+                $configContent
+            );
+        }
+
+        if (strpos($configContent, self::CONFIG_PHP_REQUIRE) === false) {
+            $configContent = str_replace(
+                $search,
+                self::CONFIG_PHP_REQUIRE . "\r\n" . $search,
+                $configContent
+            );
+        }
+
+        file_put_contents($this->getConfigPath(), $configContent);
+    }
+
+    private function removeConfigStoreClass()
+    {
+        $configContent = $this->getConfigContent();
+
+        $configContent = str_replace(
+            self::CONFIG_PHP_REQUIRE,
+            '',
+            $configContent
+        );
+
+        $configContent = str_replace(
+            "'" . self::CONFIG_STORE_CLASS . "'",
+            'null',
+            $configContent
+        );
+
+        file_put_contents($this->getConfigPath(), $configContent);
+    }
+}
